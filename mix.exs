43ccdef73e97470340ad0fd789092305d8bb2477
Suren Kirakosyan <surenkirakosyan.am@gmail.com>
2022-03-26 22:33:12 +0400

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -20,6 +20,8 @@ defmodule Opaque.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
   end
 end
