e1ab13331d071457ca9594a7446b37776fa3f8cf
Suren Kirakosyan <surenkirakosyan.am@gmail.com>
2022-03-26 22:54:54 +0400

    Add simple entity

--- lib/cpu.ex
+++ lib/cpu.ex
@@ -1,2 +1,30 @@
 defmodule CPUEntity do
+  @type t() :: %__MODULE__{
+          id: non_neg_integer(),
+          cpu_specifications: map(),
+          hash: String.t()
+        }
+
+  defstruct [
+    :id,
+    :cpu_specifications,
+    :hash
+  ]
+
+  @spec new(non_neg_integer(), map(), String.t()) :: t()
+  def new(id, cpu_specifications, hash) do
+    %__MODULE__{
+      id: id,
+      cpu_specifications: cpu_specifications,
+      hash: hash
+    }
+  end
+
+  def run() do
+    id = 32
+    cpu_specifications = %{}
+    hash = "c7a9f84bb5ac28e434238294999c298637e77cce"
+
+    new(id, cpu_specifications, hash)
+  end
 end
